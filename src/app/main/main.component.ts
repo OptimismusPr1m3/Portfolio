74334f61490b7b0061f9b36d6f05f1e63c84997e
Sebastian Wolff <sebastian.wolff@iu-study.org>
2024-01-30 23:51:20 +0100

    added welcome section with its shapes + styling

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { WelcomeComponent } from '../welcome/welcome.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [CommonModule,HeaderComponent, WelcomeComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
