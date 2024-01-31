600ee7974e2288b37ea2b88f7c9282b296fa598b
Sebastian Wolff <sebastian.wolff@iu-study.org>
2024-01-31 21:08:38 +0100

    some css fixes i.e. margin left and right, added about me section , not finished jet!

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { WelcomeComponent } from '../welcome/welcome.component';
 import { CommonModule } from '@angular/common';
+import { AboutmeComponent } from '../aboutme/aboutme.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule,HeaderComponent, WelcomeComponent],
+  imports: [CommonModule,HeaderComponent, WelcomeComponent, AboutmeComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
