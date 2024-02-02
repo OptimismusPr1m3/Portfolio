bc3c26243d26511eb13185a7653c10229552665e
Sebastian Wolff <sebastian.wolff@iu-study.org>
2024-02-02 12:34:17 +0100

    started porttfolio section, todo-> doing these projects cards

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from '../header/header.component';
 import { WelcomeComponent } from '../welcome/welcome.component';
 import { CommonModule } from '@angular/common';
 import { AboutmeComponent } from '../aboutme/aboutme.component';
+import { PfsectionComponent } from '../pfsection/pfsection.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule,HeaderComponent, WelcomeComponent, AboutmeComponent],
+  imports: [CommonModule,HeaderComponent, WelcomeComponent, AboutmeComponent, PfsectionComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
---
+++ src/app/pfsection/pfsection.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pfsection',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pfsection.component.html',
+  styleUrl: './pfsection.component.scss'
+})
+export class PfsectionComponent {
+
+}
