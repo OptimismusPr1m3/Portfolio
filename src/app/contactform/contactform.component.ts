53729f50ac5357f0351529121746948f76f5bc6f
Sebastian Wolff <sebastian.wolff@iu-study.org>
2024-02-03 20:50:05 +0100

    started contact section , contact form needs to finish

---
+++ src/app/contactform/contactform.component.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-contactform',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './contactform.component.html',
+  styleUrl: './contactform.component.scss'
+})
+export class ContactformComponent {
+  
+  contactData = {
+    name: "",
+    email: "",
+    message: "", 
+  }
+  
+  onSubmit() {
+    console.log(this.contactData)
+  }
+
+}
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -4,11 +4,12 @@ import { WelcomeComponent } from '../welcome/welcome.component';
 import { CommonModule } from '@angular/common';
 import { AboutmeComponent } from '../aboutme/aboutme.component';
 import { PfsectionComponent } from '../pfsection/pfsection.component';
+import { ContactformComponent } from '../contactform/contactform.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule,HeaderComponent, WelcomeComponent, AboutmeComponent, PfsectionComponent],
+  imports: [CommonModule,HeaderComponent, WelcomeComponent, AboutmeComponent, PfsectionComponent, ContactformComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
