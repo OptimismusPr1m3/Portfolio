ef4e028d7260f43de24267dd8c6faf41e52a4a50
Sebastian Wolff <sebastian.wolff@iu-study.org>
2024-02-03 17:12:49 +0100

    added ts for portfolio section, still need new images for projects

--- src/app/pfsection/pfsection.component.ts
+++ src/app/pfsection/pfsection.component.ts
@@ -16,5 +16,30 @@ export class PfsectionComponent {
     '/assets/images/projectframes/Pollo loco 3.png',
     '/assets/images/projectframes/Pollo loco 4.png',
   ]
+  portfolioGitLinks: string[] = [
+    'https://github.com/OptimismusPr1m3/JumpNRun_Sparkie',
+    'https://github.com/StephanieWetzel/Join',
+    'https://github.com/OptimismusPr1m3/JumpNRun_Sparkie',
+    'https://github.com/OptimismusPr1m3/JumpNRun_Sparkie'
+  ]
+  portfolioTitles: string []= [
+    'Sharkie',
+    'Join',
+    'Pokedéx',
+    'Placeholder'
+  ]
+  portfolioInfos: string[] = [
+    'Placeholder',
+    'Placeholder',
+    'Placeholder',
+    'Placeholder',
+  ]
+  portfolioTech: string[] = [
+    'Javascript | HTML | CSS',
+    'Javascript | HTML | CSS',
+    'Placeholder',
+    'Placeholder',
+  ]
+
 
 }
