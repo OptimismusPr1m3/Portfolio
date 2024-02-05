8bda55c65e59b9e7b07e5e825b58a785c914c973
Sebastian Wolff <sebastian.wolff@iu-study.org>
2024-02-05 17:17:48 +0100

    some tweaks at form with ts and css , still need a better unterstanding for it

--- src/app/contactform/contactform.component.ts
+++ src/app/contactform/contactform.component.ts
@@ -1,24 +1,41 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contactform',
   standalone: true,
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contactform.component.html',
-  styleUrl: './contactform.component.scss'
+  styleUrl: './contactform.component.scss',
 })
 export class ContactformComponent {
-  
+  isInvalidName: boolean = false;
   contactData = {
-    name: "",
-    email: "",
-    message: "", 
-  }
-  
-  onSubmit() {
-    console.log(this.contactData)
+    name: '',
+    email: '',
+    message: '',
+  };
+  isInvalidMail: boolean = false;
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.valid && ngForm.submitted) {
+      console.log(this.contactData);
+    } else if (!ngForm.valid && ngForm.submitted) {
+      this.checkNameValidation();
+      this.checkEmailValidation();
+    }
   }
 
+  checkNameValidation() {
+    if (this.contactData.name == '') {
+      this.isInvalidName = true;
+    }
+  }
+
+  checkEmailValidation() {
+    if (this.contactData.email == '') {
+      this.isInvalidMail = true;
+    }
+  }
 }
