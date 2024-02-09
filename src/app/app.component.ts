3bac60f0b6dfd1b53e71a45046dcda29f9a3387a
Sebastian Wolff <sebastian.wolff@iu-study.org>
2024-02-09 19:10:29 +0100

    added footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,RouterOutlet, MainComponent],
+  imports: [CommonModule,RouterOutlet, MainComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -5,14 +5,20 @@ import { CommonModule } from '@angular/common';
 import { AboutmeComponent } from '../aboutme/aboutme.component';
 import { PfsectionComponent } from '../pfsection/pfsection.component';
 import { ContactformComponent } from '../contactform/contactform.component';
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule,HeaderComponent, WelcomeComponent, AboutmeComponent, PfsectionComponent, ContactformComponent],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    WelcomeComponent,
+    AboutmeComponent,
+    PfsectionComponent,
+    ContactformComponent,
+  ],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
-export class MainComponent {
-
-}
+export class MainComponent {}
