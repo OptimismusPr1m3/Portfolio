3b3b64b6d00f7a02b1540522ac188feecb8e55d1
Sebastian Wolff <sebastian.wolff@iu-study.org>
2024-02-24 23:47:19 +0100

    added nav bar for mobile devices, added css + css fixes , added ts + ng specific logic at html, put header parts into separate component

---
+++ src/app/shared/headertemplate/headertemplate.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+
+@Component({
+  selector: 'app-headertemplate',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './headertemplate.component.html',
+  styleUrl: './headertemplate.component.scss'
+})
+export class HeadertemplateComponent {
+  aboutMe: boolean = false;
+  skills: boolean = false;
+  portfolio: boolean = false;
+  contact: boolean = false;
+
+  @Input() barIsOpen: boolean | undefined;
+  @Output() barHasClosed: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  toggleActiveLink(aCategoryName: string) {
+    this.aboutMe = aCategoryName === 'aboutMe';
+    this.skills = aCategoryName === 'skills';
+    this.portfolio = aCategoryName === 'portfolio';
+    this.contact = aCategoryName === 'contact';
+  }
+
+  emitCloseBarEvent() {
+    this.barHasClosed.emit(true);
+  }
+
+}
